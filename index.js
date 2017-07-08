4ed977a96eb49ec37b03cb3d808e6d90fa42c05c
Anusha Dharmasena <nushydude@gmail.com>
2017-07-09 09:34:47 +1000

    Hook up graphql with express

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var path = require('path');
+var expressGraphQL = require('express-graphql');
 
 var app = express();
 var port = 4000;
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true,
+}));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
