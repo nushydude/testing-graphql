c801824778715858e8654a417792ca9078747e59
Anusha Dharmasena <nushydude@gmail.com>
2017-07-09 11:22:01 +1000

    Create simple GraphQL schema

---
+++ schema/schema.js
@@ -0,0 +1,40 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const {
+  GraphQLInt,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} = graphql;
+
+const users = [
+  { id: '23', firstname: 'Bill', age: 20 },
+  { id: '47', firstname: 'Samantha', age: 21 },
+];
+
+const UserType = new GraphQLObjectType({
+  name: 'User', // required
+  fields: { // required
+    id: { type: GraphQLString },
+    firstname: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
